60f22b7305b266fd76ddb6bfbc3886c1a80650ee
João Paulo <paulocorrentinense@gmail.com>
2022-12-08 23:01:04 -0300

    Modal

---
+++ fe/src/components/OrderModal/index.tsx
@@ -0,0 +1,15 @@
+import { Overlay } from './styles';
+
+interface OrderModalProps {
+    visible: boolean;
+}
+
+export function OrderModal({ visible }: OrderModalProps) {
+  if (!visible) {
+    return null;   }
+  return (
+    <Overlay>
+      <h1>OrderModal</h1>
+    </Overlay>
+  );
+}
--- fe/src/components/Orders/index.tsx
+++ fe/src/components/Orders/index.tsx
@@ -1,59 +1,54 @@
-import { Container, Board, OrdersContainer } from './styles';
+import { Order } from '../../types/Order';
+import { OrdersBoard } from '../OrdersBoard';
+import { Container } from './styles';
+
+const orders: Order[] = [
+  {
+    '_id': '6383dcc56789b7d9d4058523',
+    'table': '123',
+    'status': 'WAITING',
+    'products': [
+      {
+        'product': {
+          'name': 'Pizza quatro queijos',
+          'imagePath': '1669438152115 - quatro-queijos.png',
+          'price': 40,
+        },
+        'quantity': 3,
+        '_id': '6383dcc56789b7d9d4058524'
+      },
+      {
+        'product': {
+          'name': 'Amstel',
+          'imagePath': '1669583609345 - cerveja.png',
+          'price': 5,
+        },
+        'quantity': 2,
+        '_id': '6383dcc56789b7d9d4058525'
+      }
+    ],
+  }
+];
 
 export function Orders() {
   return (
     <Container>
-      <Board>
-        <header>
-          <span>🕐</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
-        <OrdersContainer>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-        </OrdersContainer>
-      </Board>
-      <Board>
-        <header>
-          <span>🕐</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
-        <OrdersContainer>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-        </OrdersContainer>
-      </Board>
-      <Board>
-        <header>
-          <span>🕐</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
-        <OrdersContainer>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-          <button type='button'>
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-        </OrdersContainer>
-      </Board>
+      <OrdersBoard
+        icon="🕐"
+        title="Fila de espera"
+        orders={orders}
+      />
+      <OrdersBoard
+        icon="👨‍🍳"
+        title="Em preparação"
+        orders={[]}
+      />
+      <OrdersBoard
+        icon="✅"
+        title="Pronto"
+        orders={[]}
+
+      />
     </Container>
   );
 }
